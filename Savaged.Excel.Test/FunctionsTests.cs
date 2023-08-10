58f9215778c60c7a601141c151d838577f86de66
savaged <savagedavid@gmail.com>
2023-08-10 11:53:31 +0100

    Enhanced validation

--- Savaged.Excel.Test/FunctionsTests.cs
+++ Savaged.Excel.Test/FunctionsTests.cs
@@ -8,6 +8,15 @@ namespace Savaged.Excel.Test
             Assert.False(Functions.IsLeapYear(2023));
             Assert.True(Functions.IsLeapYear(2024));
             Assert.False(Functions.IsLeapYear(2025));
+            Assert.False(Functions.IsLeapYear(5));
+            Assert.False(Functions.IsLeapYear(-2023));
+            Assert.False(Functions.IsLeapYear(0));
+
+            Assert.False(Functions.IsWithinLeapYear(new DateTime(2022, 4, 6)));
+            Assert.True(Functions.IsWithinLeapYear(new DateTime(2023, 4, 6)));
+            Assert.False(Functions.IsWithinLeapYear(new DateTime(2024, 4, 6)));
+            Assert.False(Functions.IsWithinLeapYear(DateTime.MinValue));
+            Assert.False(Functions.IsWithinLeapYear(DateTime.MaxValue));
         }
 
         [Fact]
@@ -16,6 +25,9 @@ namespace Savaged.Excel.Test
             Assert.Equal(365, Functions.DaysInYear(2023));
             Assert.Equal(366, Functions.DaysInYear(2024));
             Assert.Equal(365, Functions.DaysInYear(2025));
+            Assert.Equal(365, Functions.DaysInYear(5));
+            Assert.Equal(0, Functions.DaysInYear(-2025));
+            Assert.Equal(0, Functions.DaysInYear(0));
         }
 
         [Fact]
@@ -24,6 +36,10 @@ namespace Savaged.Excel.Test
             Assert.Equal(365, Functions.DaysInYearStarting(new DateTime(2022, 4, 6)));
             Assert.Equal(366, Functions.DaysInYearStarting(new DateTime(2023, 4, 6)));
             Assert.Equal(365, Functions.DaysInYearStarting(new DateTime(2024, 4, 6)));
+            Assert.Equal(365, Functions.DaysInYearStarting(new DateTime(9998, 12, 31)));
+            Assert.Equal(365, Functions.DaysInYearStarting(DateTime.MinValue));
+            Assert.Equal(0, Functions.DaysInYearStarting(new DateTime(9999, 1, 1)));
+            Assert.Equal(0, Functions.DaysInYearStarting(DateTime.MaxValue));
         }
     }
 }
--- Savaged.Excel/Functions.cs
+++ Savaged.Excel/Functions.cs
@@ -5,14 +5,34 @@ namespace Savaged.Excel;
 
 public static class Functions
 {
-    [ExcelFunction(Description = "Is the given year a leap year (TRUE/FALSE).")]
-    public static bool IsLeapYear(int year) => DateTime.IsLeapYear(year);
-
     [ExcelFunction(Description = "Gets the number of days for a given year.")]
-    public static int DaysInYear(int year) => CultureInfo.CurrentCulture.Calendar.GetDaysInYear(year);
-
+    public static int DaysInYear(int year) =>
+        SafelyExecute(y => CultureInfo.CurrentCulture.Calendar.GetDaysInYear(y), year);
 
     [ExcelFunction(Description = "Gets the number of days starting from a given date.")]
-    public static int DaysInYearStarting(DateTime date) => (date.AddYears(1) - date).Days;
+    public static int DaysInYearStarting(DateTime date) =>
+        SafelyExecute(d => (date.AddYears(1) - d).Days, date);
 
+    [ExcelFunction(Description = "Is the given year a leap year (TRUE/FALSE).")]
+    public static bool IsLeapYear(int year) =>
+        SafelyExecute(y => DateTime.IsLeapYear(y), year);
+
+    [ExcelFunction(Description = "Is the given date within a leap year (TRUE/FALSE).")]
+    public static bool IsWithinLeapYear(DateTime date) =>
+        SafelyExecute(d => DaysInYearStarting(d) == 366, date);
+
+    private static O SafelyExecute<I,O>(Func<I,O> f, I input)
+        where O : struct
+    {
+        O output;
+        try
+        {
+            output = f(input);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            output = default;
+        }
+        return output;
+    }
 }
