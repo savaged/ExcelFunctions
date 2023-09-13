1298cb08471909c690dea801379c5b0def58b2a2
DavidSavage <david.savage@assure-consulting.co.uk>
2023-09-13 08:47:42 +0100

    tested

--- Savaged.Excel.Test/MathFunctionsTests.cs
+++ Savaged.Excel.Test/MathFunctionsTests.cs
@@ -1,10 +1,20 @@
-﻿namespace Savaged.Excel.Test;
+﻿using ExcelDna.Integration;
+
+namespace Savaged.Excel.Test;
 
 public class MathFunctionsTests
 {
     [Fact]
     public void TestMultiplyMatrices()
     {
-        // TODO
+        double[,] lhs = { { 1d, 4d, 2d }, { 3d, 6d, 8d } };
+        double[,] rhs = { { 4d, 2d, 1d }, { 6d, 5d, 3d } };
+        Assert.Equal(ExcelError.ExcelErrorValue, MathFunctions.MultiplyMatrices(lhs, rhs));
+
+        lhs = new double[,] { { 1d, 4d }, { 3d, 6d } };
+        rhs = new double[,] { { 4d, 2d, 8d }, { 6d, 5d, 7d } };
+        double[,] expectation = { { 28d, 22d, 36d }, { 48d , 36d, 66d } };
+        var result = MathFunctions.MultiplyMatrices(lhs, rhs);
+        Assert.Equal(expectation, result);
     }
 }
