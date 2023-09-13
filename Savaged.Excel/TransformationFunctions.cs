7779a48eddfac15250ad0897813b3832b71132f6
DavidSavage <david.savage@assure-consulting.co.uk>
2023-09-13 08:24:00 +0100

    tidy

--- Savaged.Excel/TransformationFunctions.cs
+++ Savaged.Excel/TransformationFunctions.cs
@@ -4,12 +4,12 @@ namespace Savaged.Excel;
 
 public static class TransformationFunctions
 {
-    [ExcelFunction(Description = "Converts data to a Verivi unique identifier.")]
+    [ExcelFunction(Description = "Converts data to a bespoke unique identifier.")]
     public static string ToUniqueIdentifier(this string data) =>
         string.IsNullOrEmpty(data) ? string.Empty :
         Convert.ToBase64String(Encoding.UTF8.GetBytes(data));
 
-    [ExcelFunction(Description = "Converts a Verivi unique identifier back to data.")]
+    [ExcelFunction(Description = "Converts a bespoke unique identifier back to data.")]
     public static string FromUniqueIdentifier(this string uid) =>
         string.IsNullOrEmpty(uid) ? string.Empty :
         Encoding.UTF8.GetString(Convert.FromBase64String(uid));
